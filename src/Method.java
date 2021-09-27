0331d6013fe5742aa686ac3dc2a3d8505a02e4ce
touhid1 <touhidulislam066@gmail.com>
2021-09-28 02:05:51 +0600

    Method

--- src/Method.java
+++ src/Method.java
@@ -1,3 +1,13 @@
 public class Method {
-    
+    public static void main(String[] args){
+        int firstFloatNumber =44;
+        int secondFloatNumber =333;
+        boolean success = myTest(firstFloatNumber,secondFloatNumber);
+        System.out.println(success);
+
+    }
+    static public boolean myTest( int firstFloatNumber , int secondFloatNumber){
+        boolean answer=firstFloatNumber>secondFloatNumber;
+        return answer;
+    }
 }
