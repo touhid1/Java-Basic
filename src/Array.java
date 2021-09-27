92d1295a208c4fb3f15b1b3961f5c8fcb6366ee1
touhid1 <touhidulislam066@gmail.com>
2021-09-28 01:47:49 +0600

    Array

--- src/Array.java
+++ src/Array.java
@@ -1,3 +1,8 @@
 public class Array {
-    
+    public static void main(String[] args){
+        int[] Cards = {5,3,68,65,9,0 };
+        String[] Names = { "Touhidul" ,"Sakib","Rakib","salman"};
+        System.out.println(Names[0]);
+        System.out.println(Cards[2]);
+    }
 }
