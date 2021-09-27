6a9942218a322a663a89b48bf02ac164da0f7f21
touhid1 <touhidulislam066@gmail.com>
2021-09-27 11:58:05 +0600

    Basic-java file create

---
+++ src/Comments.java
@@ -0,0 +1,5 @@
+public class Comments{
+    public static void main(String[] args){
+    System.out.println("Touhidul");
+    }
+}
---
+++ src/app.java
@@ -0,0 +1,12 @@
+public class app {
+    public static void main(String[] args)  {
+        System.out.println("Hello,Md Touhidul Islam! \n 01858775779  \n Bsc In CSC");
+        // System.out.println("1 \t 2");
+        // System.out.println("Touhidul \\ islam");
+        System.out.println("Touhidul \'s");
+        System.out.println("Touhidul \n islam");
+        // System.out.println("Touhidul \r islam");
+        
+    }
+}
+
