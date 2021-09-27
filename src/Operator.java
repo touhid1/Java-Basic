75651c4e38068e7db38bc7a9d23384379e70a5fd
touhid1 <touhidulislam066@gmail.com>
2021-09-28 01:38:20 +0600

    Operator

--- src/Operator.java
+++ src/Operator.java
@@ -1,3 +1,21 @@
 public class Operator {
-    
+    public static void main(String[] args) {
+        int x , y ,z, result ;
+        x=65;
+        y=63;
+        z=3;
+        y=z+50;
+        y++;
+        x--;
+        // y=+20;
+        // z=-77;
+        result =x+y/z*2%3-x;
+        System.out.println(result);
+
+        boolean results =4>8;
+        System.out.println(results);
+
+        boolean Result = (54+6)>(6+98);
+        System.out.println(Result);
+    }
 }
