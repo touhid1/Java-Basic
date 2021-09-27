7b7092fe42492cf148804925c2b64127aadb2172
touhid1 <touhidulislam066@gmail.com>
2021-09-28 01:14:23 +0600

    Variable

--- src/Variable.java
+++ src/Variable.java
@@ -1,3 +1,28 @@
 public class Variable {
-    
+    public static void main(String[] args) {
+        short firstShortNumber ;
+        int secondIntNumber ;
+        long thirdLongNumber ;
+        firstShortNumber = 4;
+        secondIntNumber = 70;
+        thirdLongNumber =67555;
+        System.out.println(firstShortNumber);
+        System.out.println(secondIntNumber);
+        System.out.println(thirdLongNumber);
+        int result = secondIntNumber + 3333 ;
+        System.out.println(result);
+        float firstFloatNumber ;
+        firstFloatNumber =57;
+        float secondFloat = firstFloatNumber/6;
+        System.out.println(secondFloat);
+        double firstDoubleNumber ;
+        firstDoubleNumber = 6543;
+        double secondDouble =firstDoubleNumber/7;
+        System.out.println(secondDouble);
+        char myCharacter ='T';
+        System.out.println(myCharacter);
+        boolean first = true;
+        System.out.println(first);
+
+    }
 }
