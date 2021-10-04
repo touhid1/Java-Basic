3cae47bfb20f07d8648f9c90066817fd4106e3b0
touhid1 <touhidulislam066@gmail.com>
2021-10-04 23:25:57 +0600

    logical And opertor

--- src/LogicalOperatorPartTow.java
+++ src/LogicalOperatorPartTow.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class LogicalOperatorPartTow {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char ch;
+        System.out.print("Enter a character : ");
+        ch = input.next().charAt(0);
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("This is a small letter");
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("This is a Capatile letter");
+        } else {
+            System.out.println("This is not a number");
+        }
+
+    }
 }
