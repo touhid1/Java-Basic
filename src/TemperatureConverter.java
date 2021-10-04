5e276c7f51561559e4792e0063d7cc3acb7df649
touhid1 <touhidulislam066@gmail.com>
2021-10-04 12:23:48 +0600

    Temprature Converter

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
-    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double celsius , Fahrenheit;
+        System.out.print("input Fahrenheit : ");
+        Fahrenheit =input.nextDouble();
+        celsius = .55*Fahrenheit-32;
+        System.out.println("celsius : "+celsius);
+    }
 }
