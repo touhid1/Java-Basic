07ba8d47e8b083eabaf8aed588d6d788f9e0d994
touhid1 <touhidulislam066@gmail.com>
2021-10-04 22:31:20 +0600

    Checking Even Odd Number

--- src/CheckingEvenOddNumber.java
+++ src/CheckingEvenOddNumber.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class CheckingEvenOddNumber {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.print("Enter a Positive number : ");
+        num = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+    }
 }
