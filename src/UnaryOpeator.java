b69a2357e280deb7c9ad5f54a936396f3b0900a9
touhid1 <touhidulislam066@gmail.com>
2021-10-04 12:52:29 +0600

    Unary Opeatror

--- src/UnaryOpeator.java
+++ src/UnaryOpeator.java
@@ -1,3 +1,15 @@
 public class UnaryOpeator {
-    
+    public static void main(String[] args){
+        int x =45;
+        int y =8;
+        int z = -x;
+        int a =++y;
+        int b =--x;
+        // int c =y--;
+        // int d =x++;
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
 }
