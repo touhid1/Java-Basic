b31eb3a7db6ac304d48661b5085477fad716039e
touhid1 <touhidulislam066@gmail.com>
2021-10-04 11:42:17 +0600

    done

--- src/AritmeticOperator.java
+++ src/AritmeticOperator.java
@@ -1,3 +1,19 @@
 public class AritmeticOperator {
+    public static void main(String[] args){
+        int x =4;
+        int y =7;
+        int z =78;
+        int a =x+y;
+        int c =z-a;
+        int d =x*y;
+        int e =z/c;
+        int f = z%y;
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
     
 }
