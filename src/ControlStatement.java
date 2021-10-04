dea6005326e0070da22d39c8a84656d7d50c5a8e
touhid1 <touhidulislam066@gmail.com>
2021-10-04 22:23:12 +0600

    Contorl Statement

--- src/ControlStatement.java
+++ src/ControlStatement.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class ControlStatement {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.print("Please enter a number: ");
+        num = input.nextInt();
+        /*
+         * If ConditionalOperator // if (num>0) { // System.out.println("Postive"); }
+         */
+
+        /*
+         * if (num>0) { System.out.println("Positive"); } else {
+         * System.out.println("Negative"); }
+         */
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
 }
