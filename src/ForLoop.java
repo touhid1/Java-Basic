b7d45398eaa2c925b66a3e69f2753fc761a4fe3c
touhid1 <touhidulislam066@gmail.com>
2021-10-07 01:17:52 +0600

    For loop

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,3 +1,8 @@
 public class ForLoop {
-    
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Dilsad vai ");
+        }
+    }
+
 }
