01ecd637e892c04779b2166f288e51af5995397f
touhid1 <touhidulislam066@gmail.com>
2021-10-06 22:20:09 +0600

    Swith Oprator

--- src/SwitchPartOne.java
+++ src/SwitchPartOne.java
@@ -1,3 +1,45 @@
+import java.util.Scanner;
+
 public class SwitchPartOne {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int digit;
+        System.out.print("Enter a Positive number : ");
+        digit = input.nextInt();
+        switch (digit) {
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Tow");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Fore");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+                System.out.println("Are vai tui ai da ki dicoc");
+        }
+    }
+
 }
