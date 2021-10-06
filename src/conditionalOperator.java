ceba305290bc7d23d67f0af82cc99f6c1e8ec192
touhid1 <touhidulislam066@gmail.com>
2021-10-06 22:29:10 +0600

    conditional Operator

--- src/conditionalOperator.java
+++ src/conditionalOperator.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class conditionalOperator {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x, y, large;
+        System.out.println("Enter a Positive number : ");
+        x = input.nextInt();
+        y = input.nextInt();
+        large = (x > y) ? x : y;
+        System.out.println("The largest number = " + large);
+
+    }
+
 }
