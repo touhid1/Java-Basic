7cb519a869df1ec375d098cd52a4d9f7ba09288e
touhid1 <touhidulislam066@gmail.com>
2021-10-07 10:46:46 +0600

    Factorial number

--- src/FactorialOfaNumber.java
+++ src/FactorialOfaNumber.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class FactorialOfaNumber {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, sum = 1;
+        System.out.print("Enter a number : ");
+        n = input.nextInt();
+        for (int i = n; i >= 1; i--) {
+            sum = sum * i;
+            // System.out.println(sum);
+        }
+        System.out.println("Result = " + sum);
+    }
 }
