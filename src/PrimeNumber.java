b7d9bd1d85352b70218f066a8833270dbe11ec73
touhid1 <touhidulislam066@gmail.com>
2021-10-08 00:11:58 +0600

    Prime Number

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class PrimeNumber {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Plese Enter a number");
+        int num = input.nextInt();
+        int count = 0;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                count++;
+                break;
+            }
+        }
+        if (count == 0) {
+            System.out.println("This is a prime number");
+        } else {
+            System.out.println("This is a not prime number");
+        }
+    }
 }
