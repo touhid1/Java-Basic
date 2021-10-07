d21cf36bc8d555092b58d06da51a17cb5a47f5a6
touhid1 <touhidulislam066@gmail.com>
2021-10-07 10:16:01 +0600

    Print Even Number

--- src/PrintEvenNumberFrom.java
+++ src/PrintEvenNumberFrom.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class PrintEvenNumberFrom {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m, n;
+        System.out.print("Enter a Positive number : ");
+        m = input.nextInt();
+        n = input.nextInt();
+        int sum = 0;
+        for (int i = m; i <= n; i++) {
+            if (i % 2 == 1)
+                sum = sum + i;
+        }
+        System.out.println(sum);
+    }
 }
