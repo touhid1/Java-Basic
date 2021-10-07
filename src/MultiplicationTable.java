a78e88bf0f32099a022147c65782c0fae3256cd6
touhid1 <touhidulislam066@gmail.com>
2021-10-07 11:15:03 +0600

    Multiplication

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num;
-        System.out.print("Enter a Positive number : ");
-        num = input.nextInt();
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(num + "x" + i + " = " + num * i);
+        int n, m;
+        System.out.print("Enter a  number : ");
+        m = input.nextInt();
+        n = input.nextInt();
+
+        /*
+         * for (int i = 1; i <= 10; i++) { System.out.println(num + "x" + i + " = " +
+         * num * i); }
+         */
+        for (int i = m; i <= n; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + "x" + j + " = " + i * j);
+            }
+            System.out.println("\n\n");
         }
     }
 }
