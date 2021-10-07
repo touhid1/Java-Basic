f056b70f2a6af0844e88b48c329177faff5ee548
touhid1 <touhidulislam066@gmail.com>
2021-10-07 10:37:56 +0600

    Serice part Tow

--- src/SericePartTow.java
+++ src/SericePartTow.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class SericePartTow {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, Result = 1;
+        System.out.print("Enter a Positive number : ");
+        n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            Result = Result * i;
+            // System.out.println(sum);
+        }
+        System.out.println("Result = " + Result);
+    }
 }
