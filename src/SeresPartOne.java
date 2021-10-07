cef635fdec0f5bb105225152ff4ce05922a43f00
touhid1 <touhidulislam066@gmail.com>
2021-10-07 10:31:55 +0600

    Seres Part one

--- src/SeresPartOne.java
+++ src/SeresPartOne.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class SeresPartOne {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, sum = 0;
+        System.out.print("Enter a Positive number : ");
+        n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i * i;
+            // System.out.println(sum);
+        }
+        System.out.print("Result = " + sum);
+    }
 }
