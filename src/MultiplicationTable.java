8eda2716622fceaba5b6476f45884d27a515d1a9
touhid1 <touhidulislam066@gmail.com>
2021-10-07 11:00:59 +0600

    Multiplication

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class MultiplicationTable {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.print("Enter a Positive number : ");
+        num = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + "x" + i + " = " + num * i);
+        }
+    }
 }
