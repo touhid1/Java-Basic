5956eb7dabf076741767e6ed087fd1bcafcbcab2
touhid1 <touhidulislam066@gmail.com>
2021-10-07 06:21:36 +0600

    Do While Loop

--- src/DoWhileLoop.java
+++ src/DoWhileLoop.java
@@ -1,3 +1,9 @@
 public class DoWhileLoop {
-    
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i = i + 2;
+        } while (i <= 100);
+    }
 }
