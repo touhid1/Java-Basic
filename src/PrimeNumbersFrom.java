b4e462217da668d5687519bc2756ca055c26491c
touhid1 <touhidulislam066@gmail.com>
2021-10-08 23:26:56 +0600

    Prime Number From m to n

--- src/PrimeNumbersFrom.java
+++ src/PrimeNumbersFrom.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class PrimeNumbersFrom {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m, n, count = 0, totalPrimeNumber = 0;
+        System.out.println("Plese Enter a number");
+        m = input.nextInt();
+        System.out.println("Plese Enter a number");
+        n = input.nextInt();
+        for (int i = m; i <= n; i++) {
+            for (int j = 2; j <= i - 1; j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.println(i);
+                totalPrimeNumber++;
+            }
+            count = 0;
+        }
+    }
 }
