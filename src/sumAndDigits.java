1429c02b244aabe554cf8d247abba5593027d8dd
touhid1 <touhidulislam066@gmail.com>
2021-10-09 15:26:07 +0600

    Sum and Digits

--- src/sumAndDigits.java
+++ src/sumAndDigits.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class sumAndDigits {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num, temp, r, sum = 0;
+        System.out.print("Enter a Positive number : ");
+        num = input.nextInt();
+        temp = num;
+
+        while (temp != 0) {
+            r = temp % 10;
+            sum = sum + r;
+            temp = temp / 10;
+
+        }
+        System.out.println(sum);
+    }
 }
