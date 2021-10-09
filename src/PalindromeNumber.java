c52b163cbca7d938ddb7d0210243efd42603d6ec
touhid1 <touhidulislam066@gmail.com>
2021-10-09 15:42:38 +0600

    Palindrome number

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class PalindromeNumber {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num, temp, r, sum = 0;
+        System.out.print("Enter a Positive number : ");
+        num = input.nextInt();
+        temp = num;
+
+        while (temp != 0) {
+            r = temp % 10;
+            sum = sum * 10 + r;
+            temp = temp / 10;
+
+        }
+        if (num == sum) {
+            System.out.println("This is Palindrome Number");
+        } else {
+            System.out.println("This is not a Palindrome Number");
+        }
+    }
 }
