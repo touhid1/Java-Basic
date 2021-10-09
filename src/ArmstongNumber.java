3705c21daac5760cac0c25843b4cc26d808dc804
touhid1 <touhidulislam066@gmail.com>
2021-10-09 15:52:30 +0600

    Armstong Number

--- src/ArmstongNumber.java
+++ src/ArmstongNumber.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class ArmstongNumber {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num, temp, r, sum = 0;
+        System.out.print("Enter a Positive number : ");
+        num = input.nextInt();
+        temp = num;
+
+        while (temp != 0) {
+            r = temp % 10;
+            sum = sum + r * r * r;
+            temp = temp / 10;
+
+        }
+        if (num == sum) {
+            System.out.println("This is Armstong Number");
+        } else {
+            System.out.println("This is not Armstong Number");
+        }
+
+    }
 }
