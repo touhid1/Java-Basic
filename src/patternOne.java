da7672192153dcfbcaf0e800471374e9d63a50d1
touhid1 <touhidulislam066@gmail.com>
2021-10-09 16:13:16 +0600

    pattern

--- src/patternOne.java
+++ src/patternOne.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ParrernOne {
+public class patternOne {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n;
