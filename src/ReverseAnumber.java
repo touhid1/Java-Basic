a1bbee53a71f91fd8e4139e0f12f516c4ae34cce
touhid1 <touhidulislam066@gmail.com>
2021-10-09 15:33:09 +0600

    Reverse An number

--- src/ReverseAnumber.java
+++ src/ReverseAnumber.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class ReverseAnumber {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num, temp, r, sum = 0;
+        System.out.print("Enter a Positive number : ");
+        num = input.nextInt();
+        temp = num;
+
+        while (temp != 0) {
+            r = temp % 10;
+            sum = sum * 10 + r;
+            temp = temp / 10;
+
+        }
+        System.out.println(sum);
+    }
 }
