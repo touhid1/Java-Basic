9f887fedea08a0bf48bcc13abea87d3bf381534f
touhid1 <touhidulislam066@gmail.com>
2021-10-09 15:31:12 +0600

    Sum of Digits

--- src/sumofDigits.java
+++ src/sumofDigits.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class sumAndDigits {
+public class sumofDigits {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num, temp, r, sum = 0;
