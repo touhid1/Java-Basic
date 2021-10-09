34731bedfa3b2f8e61cc180512057ac6c2ceffa8
touhid1 <touhidulislam066@gmail.com>
2021-10-09 16:10:52 +0600

    pattern

--- src/ParrernOne.java
+++ src/ParrernOne.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class ParrernOne {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.println("Plese Enter a colum");
+        n = input.nextInt();
+        for (int col = 1; col <= n; col++) {
+            for (int row = 1; row <= col; row++) {
+                System.out.print(" " + col);
+            }
+            System.out.println();
+        }
+    }
 }
