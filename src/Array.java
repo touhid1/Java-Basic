f61ab31491b6f4576ff3305a69be7a986109b107
touhid1 <touhidulislam066@gmail.com>
2021-10-10 22:13:18 +0600

    array

--- src/Array.java
+++ src/Array.java
@@ -1,7 +1,7 @@
 public class Array {
-    public static void main(String[] args){
-        int[] Cards = {5,3,68,65,9,0 };
-        String[] Names = { "Touhidul" ,"Sakib","Rakib","salman"};
+    public static void main(String[] args) {
+        int[] Cards = { 5, 3, 68, 65, 9, 0 };
+        String[] Names = { "Touhidul", "Sakib", "Rakib", "salman", "Sojib" };
         System.out.println(Names[0]);
         System.out.println(Cards[2]);
     }
--- src/Pattern.java
+++ src/Pattern.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-public class PatternTow {
+public class Pattern {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n;
         System.out.println("Plese Enter a colum");
-        n = input.nextInt();
-        for (int row = n; row >= 1; row--) {
+        char c = input.next().charAt(0);
+        for (int row = 1; row <= c; row++) {
             for (int col = 1; col <= row; col++) {
                 System.out.print(" " + col);
             }
