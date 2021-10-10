b5ffab4a59dbce917ee19dcae0c5f97c42934702
touhid1 <touhidulislam066@gmail.com>
2021-10-10 23:20:48 +0600

    pattern

--- src/array.java
+++ src/array.java
@@ -1,3 +1,14 @@
 public class array {
-    
+    public static void main(String[] args) {
+        int[] number = new int[4];
+        number[0] = 4;
+        number[1] = 9;
+        number[2] = 10;
+        number[3] = 21;
+        int sum = number[0] + number[1];
+        int len = number.length;
+        System.out.println(number[3]);
+        System.out.println(len);
+        System.out.println(sum);
+    }
 }
