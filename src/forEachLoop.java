de299a42ebb0a8b0167ada85169d009642eb6c83
touhid1 <touhidulislam066@gmail.com>
2021-10-11 00:07:54 +0600

    For Each Loop

--- src/forEachLoop.java
+++ src/forEachLoop.java
@@ -1,3 +1,9 @@
 public class forEachLoop {
-    
+    public static void main(String[] args) {
+        String[] Name = { "Anis", "Sakil", "Abir", "kamal" };
+        for (var x : Name) {
+            System.out.println(x);
+
+        }
+    }
 }
